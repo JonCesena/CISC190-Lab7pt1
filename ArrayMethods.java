f645231f006401e9cf894ff838bcf2b2f8dff622
Jon Cesena <joncesena@proton.me>
2026-04-27 17:39:13 -0700

    Updated with part 2 of lab

--- ArrayMethods.java
+++ ArrayMethods.java
@@ -1,6 +1,10 @@
+import java.util.Arrays;
 
 /**
- * Write a description of class ArrayMethods here.
+ * This program takes myArray and returns count, sum, average, max value, and index of
+ * max value. It then finds the right most key value, number of key values, and what
+ * indices they are at. It then copies myArray and compares the copy to myArray. It
+ * finally reverses the order of myArray and compares myArray and the reversed copy.
  *
  * @author Jon Cesena
  * @version v1.0
@@ -76,7 +80,95 @@ public class ArrayMethods
         }
         return index;
     }
+
+    /**
+     * Method to right most index of key. If not found return -1
+     * @param key value to be assessed
+     * @return right most index of key passed in
+     */
+    public int findLast(int key){
+        int index = -1;
+        for (int i = myArray.length-1;i>=0;i--){
+            if(myArray[i]==key){
+                index = i;
+                return index;
+            }
+        }
+        return index;
+    }
+
+    /**
+     * Method finds all key values in myArray and returns number of key
+     * values found
+     * @param key the value searched for in myArray
+     * @return a new array of all key values
+     */
+    public int[ ] findAll(int key){
+        int instances = 0; //number of times key is found
+        for (int i=0;i<myArray.length;i++){
+            if (myArray[i]==key){
+                instances++;
+            }
+        }
+        int[] indices = new int[instances];
+        int element = 0;
+        for (int i=0;i<myArray.length;i++){
+            if (myArray[i]==key){
+                indices[element]=i;
+                element++;
+            }
+        }
+        return indices;
+    }
+
+    /**
+     * Method returns a copy of myArray
+     * @return int copy of myArray
+     */
+    public int[] copyArray(){
+        return Arrays.copyOf(myArray,myArray.length);
+    }
+
+    /**
+     * Method returns myArray
+     * @return in of myArray
+     */
+    public int[] getArray(){
+        return myArray;
+    }
+
+    /**
+     * Method uses a temp variable to reverse the order of myArray
+     * @param inArray the array to be reversed
+     * @return new array with order reversed
+     */
+    public int[] reverseArray(int[] inArray){
+        for (int left=0,right=inArray.length-1;left<right;left++,right--){
+            int temp=inArray[left];
+            inArray[left]=inArray[right];
+            inArray[right]=temp;
+        }
+        return inArray;
+    }
+    
+    /**
+     * Method prints an int array, nicely formatted
+     * @param inArray int array to print
+     */
+    public void print(int[] inArray)
+    {
+        System.out.print("{");
+        int i;
+        // print elements before the last, separated by commas
+        for (i = 0; i < inArray.length - 1; i++)
+            System.out.print(inArray[i] + ", ");
+        // print last element. Careful here to handle length 0
+        if (inArray.length > 0)
+            System.out.print(inArray[i]);
+        System.out.println("}");
+    }
 }//end ArrayMethods
+
 
 
 
